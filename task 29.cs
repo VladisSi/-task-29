3d71c093b5f66879f79adfd2e3ab689f945601f8
Kastalom22 <kastalom22@yandex.ru>
2023-10-08 09:26:33 +0300

    код к задачи 29

---
+++ task 29.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+class Program
+{
+    static void Main()
+    {
+        // Задаем массив из 8 элементов
+        int[] numbers = { 1, 2, 5, 7, 19, 6, 1, 33 };
+
+        // Выводим массив на экран
+        Console.Write("[");
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write(numbers[i]);
+            if (i < numbers.Length - 1)
+            {
+                Console.Write(", ");
+            }
+        }
+        Console.WriteLine("]");
+    }
+}
